d728bc030c53a56f297e9f6530e5879e219d2927
PayPound <tech@paypound.ltd>
2021-10-20 14:20:29 +0530

    add seeder

---
+++ database/seeds/CreateAdminUserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Admin;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\DB;
+
+class CreateAdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = Admin::create([
+            'name' => 'Harsukh Makwana',
+            'email' => 'harsukh21@gmail.com',
+            'password' => bcrypt('123456')
+        ]);
+        $role = Role::create(['name' => 'Admin', 'guard_name' => 'admin']);
+        $permissions = Permission::pluck('id', 'id')->all();
+        $role->syncPermissions($permissions);
+        $user->assignRole([$role->id]);
+    }
+}
