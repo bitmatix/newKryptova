d680672b3e5ceaef208a4b5ba97a14f5de99726f
harshadpathak <harshadpathak13@gmail.com>
2021-10-20 15:46:18 +0530

    add menu1

--- resources/views/layouts/admin/sidefooter.blade.php
+++ resources/views/layouts/admin/sidefooter.blade.php
@@ -10,7 +10,7 @@
         <!--end::Svg Icon-->
     </button>
     <!--begin::Menu 2-->
-    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-bold w-200px" data-kt-menu="true">
+    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-bold w-250px" data-kt-menu="true">
         <!--begin::Menu item-->
         <div class="menu-item px-3">
             <div class="menu-content fs-6 text-dark fw-bolder px-3 py-4">Quick Actions</div>
@@ -20,59 +20,69 @@
         <div class="separator mb-3 opacity-75"></div>
         <!--end::Menu separator-->
         <!--begin::Menu item-->
-        <div class="menu-item px-3">
-            <a href="#" class="menu-link px-3">New Ticket</a>
-        </div>
-        <!--end::Menu item-->
-        <!--begin::Menu item-->
-        <div class="menu-item px-3">
-            <a href="#" class="menu-link px-3">New Customer</a>
-        </div>
-        <!--end::Menu item-->
-        <!--begin::Menu item-->
         <div class="menu-item px-3" data-kt-menu-trigger="hover" data-kt-menu-placement="right-start">
             <!--begin::Menu item-->
             <a href="#" class="menu-link px-3">
-                <span class="menu-title">New Group</span>
+                <span class="menu-title">Technical & Additional</span>
                 <span class="menu-arrow"></span>
             </a>
             <!--end::Menu item-->
             <!--begin::Menu sub-->
-            <div class="menu-sub menu-sub-dropdown w-175px py-4">
+            <div class="menu-sub menu-sub-dropdown w-250px py-4">
                 <!--begin::Menu item-->
                 <div class="menu-item px-3">
-                    <a href="#" class="menu-link px-3">Admin Group</a>
+                    <a href="#" class="menu-link px-3">IP Whitelist</a>
                 </div>
                 <!--end::Menu item-->
                 <!--begin::Menu item-->
                 <div class="menu-item px-3">
-                    <a href="#" class="menu-link px-3">Staff Group</a>
+                    <a href="#" class="menu-link px-3">Transaction Session Data</a>
                 </div>
-                <!--end::Menu item-->
-                <!--begin::Menu item-->
+
                 <div class="menu-item px-3">
-                    <a href="#" class="menu-link px-3">Member Group</a>
+                    <a href="#" class="menu-link px-3">Required Fields</a>
+                </div>
+
+                <div class="menu-item px-3">
+                    <a href="#" class="menu-link px-3">Merchant & RP Agreement</a>
+                </div>
+
+                <div class="menu-item px-3">
+                    <a href="#" class="menu-link px-3">Payout Schedule</a>
+                </div>
+
+                <div class="menu-item px-3">
+                    <a href="#" class="menu-link px-3">Industry Type</a>
+                </div>
+
+                <div class="menu-item px-3">
+                    <a href="#" class="menu-link px-3">Integration Preference</a>
+                </div>
+
+                <div class="menu-item px-3">
+                    <a href="#" class="menu-link px-3">Admin Logs</a>
+                </div>
+
+                <div class="menu-item px-3">
+                    <a href="#" class="menu-link px-3">Mail Templates</a>
                 </div>
                 <!--end::Menu item-->
             </div>
             <!--end::Menu sub-->
         </div>
         <!--end::Menu item-->
+
         <!--begin::Menu item-->
         <div class="menu-item px-3">
-            <a href="#" class="menu-link px-3">New Contact</a>
+            <a href="#" class="menu-link px-3">New Ticket</a>
         </div>
         <!--end::Menu item-->
-        <!--begin::Menu separator-->
-        <div class="separator mt-3 opacity-75"></div>
-        <!--end::Menu separator-->
         <!--begin::Menu item-->
         <div class="menu-item px-3">
-            <div class="menu-content px-3 py-3">
-                <a class="btn btn-primary btn-sm px-4" href="#">Generate Reports</a>
-            </div>
+            <a href="#" class="menu-link px-3">Upload Agreement</a>
         </div>
         <!--end::Menu item-->
+
     </div>
     <!--end::Menu 2-->
 </div>
